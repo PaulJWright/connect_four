65c59288a65ef71077fee3e19d60a9e7c739283f
Paul Wright <paul@pauljwright.co.uk>
2024-09-12 10:12:00 +0100

    updating classes

--- connect_four/main.py
+++ connect_four/main.py
@@ -5,13 +5,38 @@ class ConnectFour():
         self.rows = 6
         self.columns = 7
         self.players = [] 
+    
+    pass
 
+    def show_grid(self):
+        """
+        display the board?
+        """
         pass
+
+    def drop_counter(self, column, counter):
+        """
+        drop counter in col
+        """
+        pass
+
+    def check_for_end(self):
+        """"
+        check if win or stalemate
+        """
+        pass
+
+    def play(self):
+        """
+        play the game
+        """
+        pass
+
+class Counter():
+    pass
 
 class Player():
     pass
 
 class HumanPlayer():
-
-def main()
-    
+    pass
