4d9bd0f5da612c44a24b01746621fec9ebb66b4b
Paul Wright <paul@pauljwright.co.uk>
2024-09-12 10:09:22 +0100

    first commit

---
+++ connect_four/main.py
@@ -0,0 +1,17 @@
+
+
+class ConnectFour():
+    def __init__(self) -> None:
+        self.rows = 6
+        self.columns = 7
+        self.players = [] 
+
+        pass
+
+class Player():
+    pass
+
+class HumanPlayer():
+
+def main()
+    
