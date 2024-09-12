ebec46679a4597c2a55ebda1aac949504be3b9e7
Paul Wright <paul@pauljwright.co.uk>
2024-09-12 10:42:20 +0100

    initial moves & pre-commit

--- connect_four/main.py
+++ connect_four/main.py
@@ -1,17 +1,26 @@
-
-from abc import ABC, abstractmethod
 import random
+from abc import ABC, abstractmethod
+
 import numpy as np
+
 # from enum import Enum
 # from pydantic import BaseModel, validator, ValidationError
 
-class ConnectFour():
+# # Configure the logger
+# logging.basicConfig(level=logging.INFO)
+# logger = logging.getLogger(__name__)
+
+
+class ConnectFour:
     def __init__(self, player_1, player_2) -> None:
         self._rows = 6
         self._columns = 7
         self._players = [player_1, player_2]
-        self._grid = np.full((self.rows, self.columns), ' ')
+        self._grid = np.full(
+            (self.rows, self.columns), " "
+        )  # return a filled grid with blank spaces
 
+        # some basic validation
         self.check_different_counters()
 
     @property
@@ -25,16 +34,18 @@ class ConnectFour():
     @property
     def players(self):
         return self._players
-    
+
     @property
     def grid(self):
         return self._grid
-    
+
+    # ensure that each player has a different counter
+    # -- probably a better way with ENUM and pydantic
     def check_different_counters(self):
         """Validator to ensure that both players have different counters."""
         counters = {player.counter for player in self._players}
         if len(counters) != len(self._players):
-            raise ValueError('Players must have different counters.')
+            raise ValueError("Players must have different counters.")
 
     def show_grid(self):
         """
@@ -44,15 +55,25 @@ class ConnectFour():
         for row in self._grid:
             print("| " + " | ".join(row) + " |")
 
-        
     def drop_counter(self, column, counter):
         """
         drop counter in col
         """
-        pass
+        if column < 0 or column >= self.columns:
+            # raise ValueError("Invalid column. Choose between 0 and 6.")
+            print("Invalid column. Choose between 0 and 6.")
+            return False
+
+        for row in reversed(self.grid):  # Start checking from the bottom row
+            if row[column] == " ":
+                row[column] = counter
+                return True
+
+        print("Column is full. Choose another column.")
+        return False
 
     def check_for_end(self):
-        """"
+        """ "
         check if win or stalemate
         """
         pass
@@ -61,11 +82,27 @@ class ConnectFour():
         """
         play the game
         """
+        player_index = 0
+        self.show_grid()  # show grid at beginning
 
         while True:
             # display grid each time
             self.show_grid()
+
+            current_player = self.players[player_index]
+            # logger.info(f"Player {player_index}")
+            print(f"Current Player: Player {player_index}")
+
+            valid_turn = False
+            while not valid_turn:
+                valid_turn = self.drop_counter(
+                    current_player.move(),
+                    current_player.counter,
+                )
+
+            self.show_grid()
             break
+
 
 class Player(ABC):
     def __init__(self, counter):
@@ -77,10 +114,12 @@ class Player(ABC):
         Method to move. Must be implemented in subclasses."""
         pass
 
+
 class HumanPlayer(Player):
     """
     human player needs to input
     """
+
     def move(self):
         while True:
             try:
@@ -89,21 +128,23 @@ class HumanPlayer(Player):
             except ValueError:
                 print("Invalid input. Please enter an integer between 0 and 6.")
 
+
 class ComputerPlayer(Player):
     """
     define a computer player; makes random moves
     """
+
     def move(self):
         return random.randint(0, 6)
+
 
 # class Counter(str, Enum):
 #     """Enumeration for valid counters in the game."""
 #     X = 'X'
 #     O = 'O'
 
-
 if __name__ == "__main__":
     # Initialize the game
-    game = ConnectFour(player_1=HumanPlayer('x'), player_2=ComputerPlayer('o'))
+    game = ConnectFour(player_1=HumanPlayer("x"), player_2=ComputerPlayer("o"))
     # Start the game
     game.play()
